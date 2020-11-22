2754d76fc322ed4b0696abc14176e9c172de3669
Lam Tran <thanhlam24101991@gmail.com>
2020-11-23 01:41:21 +0700

    add test coverage

--- tests/test_register.py
+++ tests/test_register.py
@@ -6,7 +6,7 @@ import os
 
 from config import Config
 from app import createApiApp
-from app.models import db
+from app.models import db, Country
 import json
 rootDir = os.path.abspath(os.path.dirname(__file__))
 
@@ -30,23 +30,32 @@ def destroyTestEnvironment(test):
     test.app_context.pop()
 
 
+def seedData():
+    country = Country(code='VN', name='Viet Nam')
+    db.session.add(country)
+    db.session.commit()
+
+
 class RegisterTest(unittest.TestCase):
     def setUp(self):
         self.flaskApp = setupTestEnvironment(self)
+        seedData()
         self.client = self.flaskApp.test_client()
 
     def tearDown(self):
         destroyTestEnvironment(self)
 
-    def register(self, email, password):
+    def postRegister(self, email, password, firstName, lastName, country):
         return self.client.post(
             '/auth/register',
-            data=json.dumps(dict(email=email)),
+            data=json.dumps(dict(email=email, password=password,
+                                 firstName=firstName, lastName=lastName, country=country)),
             content_type='application/json'
         )
 
-    def testRegister(self):
-        response = self.register(
-            'patkennedy79@gmail.com', 'FlaskIsAwesome')
-        self.assertEqual(response.data, '')
+    def test_register(self):
+        response = self.postRegister(
+            'lamttt@mailnesia.com', 'test password', 'lam', 'tran', 'VN')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data, b'{"email":"lamttt@mailnesia.com","firstName":"lam","id":1,"lastName":"tran"}\n')
